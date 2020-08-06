4327b10bb07ab2bbf823bc8ef3a3a3b4a7c48a80
Kato Shinya <yourdream28@gmail.com>
2020-08-06 18:42:19 +0900

    add

---
+++ src/main/java/org/thinkit/formatter/SqlFormatter.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2020 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.thinkit.formatter;
+
+import org.hibernate.jdbc.util.BasicFormatterImpl;
+import org.thinkit.formatter.common.Formatter;
+
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import lombok.ToString;
+
+/**
+ * SQLクエリを整形する処理を定義したフォーマッタクラスです。
+ *
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ */
+@ToString
+@EqualsAndHashCode
+public final class SqlFormatter implements Formatter {
+
+    /**
+     * デフォルトコンストラクタ
+     */
+    private SqlFormatter() {
+    }
+
+    /**
+     * {@link SqlFormatter} クラスの新しいインスタンスを生成し返却します。
+     *
+     * @return {@link SqlFormatter} クラスの新しいインスタンス
+     */
+    public static Formatter of() {
+        return new SqlFormatter();
+    }
+
+    @Override
+    public String format(@NonNull final String sql) {
+
+        if (sql.isEmpty()) {
+            return "";
+        }
+
+        return new BasicFormatterImpl().format(sql);
+    }
+}
