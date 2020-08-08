c7a12717ecf83313f69aad3925c9a984d6d0eb8f
Kato Shinya <yourdream28@gmail.com>
2020-08-08 17:45:26 +0900

    add

---
+++ src/main/java/org/thinkit/formatter/DmlAppender.java
@@ -0,0 +1,64 @@
+package org.thinkit.formatter;
+
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+
+@EqualsAndHashCode
+final class DmlAppender {
+
+    /**
+     * 整形済みのSQL
+     */
+    private StringBuilder sql;
+
+    /**
+     * DMLトークナイザ
+     */
+    private DmlTokenizer dmlTokenizer;
+
+    /**
+     * DMLインデンター
+     */
+    private DmlIndenter dmlIndenter;
+
+    /**
+     * デフォルトコンストラクタ
+     */
+    private DmlAppender() {
+    }
+
+    private DmlAppender(@NonNull DmlTokenizer dmlTokenizer) {
+        this.sql = new StringBuilder();
+        this.dmlTokenizer = dmlTokenizer;
+        this.dmlIndenter = DmlIndenter.of();
+    }
+
+    public static DmlAppender register(@NonNull DmlTokenizer dmlTokenizer) {
+        return new DmlAppender(dmlTokenizer);
+    }
+
+    public DmlAppender appendToken() {
+        this.sql.append(this.dmlTokenizer.getToken());
+        return this;
+    }
+
+    public DmlAppender appendNewLine() {
+        this.sql.append(this.dmlIndenter.newline());
+        return this;
+    }
+
+    public DmlAppender increment() {
+        this.dmlIndenter.increment();
+        return this;
+    }
+
+    public DmlAppender decrement() {
+        this.dmlIndenter.decrement();
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return this.sql.toString();
+    }
+}
