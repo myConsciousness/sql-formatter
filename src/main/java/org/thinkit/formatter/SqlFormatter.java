c1867324fc415ce7fa0ae90d1c2723b981b55994
Kato Shinya <yourdream28@gmail.com>
2020-08-08 17:45:48 +0900

    update

--- src/main/java/org/thinkit/formatter/SqlFormatter.java
+++ src/main/java/org/thinkit/formatter/SqlFormatter.java
@@ -14,7 +14,15 @@
 
 package org.thinkit.formatter;
 
-import org.hibernate.jdbc.util.BasicFormatterImpl;
+import java.util.LinkedList;
+
+import org.thinkit.common.catalog.Delimiter;
+import org.thinkit.formatter.catalog.DmlStatement;
+import org.thinkit.formatter.catalog.EndClause;
+import org.thinkit.formatter.catalog.LogicalExpression;
+import org.thinkit.formatter.catalog.MiscStatement;
+import org.thinkit.formatter.catalog.Quantifier;
+import org.thinkit.formatter.catalog.StartClause;
 import org.thinkit.formatter.common.Formatter;
 
 import lombok.EqualsAndHashCode;
@@ -22,7 +30,7 @@ import lombok.NonNull;
 import lombok.ToString;
 
 /**
- * SQLクエリを整形する処理を定義したフォーマッタクラスです。
+ * SQLにおけるDMLクエリを整形する処理を定義したフォーマッタクラスです。
  *
  * @author Kato Shinya
  * @since 1.0
@@ -30,7 +38,15 @@ import lombok.ToString;
  */
 @ToString
 @EqualsAndHashCode
-public final class SqlFormatter implements Formatter {
+public class SqlFormatter implements Formatter {
+
+    /**
+     * 空白
+     */
+    private static final String WHITESPACES = " \n\r\f\t";
+
+    boolean afterByOrSetOrFromOrSelect;
+    private LinkedList<Boolean> afterByOrFromOrSelects = new LinkedList<>();
 
     /**
      * デフォルトコンストラクタ
@@ -50,10 +66,188 @@ public final class SqlFormatter implements Formatter {
     @Override
     public String format(@NonNull final String sql) {
 
-        if (sql.isEmpty()) {
-            return "";
+        final StringBuilder formatted = new StringBuilder();
+
+        final DmlIndenter indenter = DmlIndenter.of();
+        final FunctionFixer function = FunctionFixer.of();
+        final ParenthesisFixer startParenthesis = ParenthesisFixer.of();
+
+        boolean beginLine = false;
+        boolean inClauses = false;
+
+        final DmlTokenizer tokenizer = DmlTokenizer.of(sql);
+        final DmlAppender appender = DmlAppender.register(tokenizer);
+
+        while (tokenizer.next()) {
+            String token = tokenizer.getToken();
+            String lowercaseToken = tokenizer.getLowercaseToken();
+            String lastToken = tokenizer.getLastToken();
+
+            if (DmlStatement.contains(lowercaseToken)) {
+
+                formatted.append(token);
+
+                if (DmlStatement.SELECT.getStatement().equals(lowercaseToken)) {
+                    formatted.append(indenter.increment().newline());
+                    startParenthesis.push();
+                    afterByOrFromOrSelects.addLast(afterByOrSetOrFromOrSelect);
+
+                    beginLine = true;
+                    afterByOrSetOrFromOrSelect = true;
+                } else {
+                    indenter.increment();
+                    beginLine = false;
+
+                    if (DmlStatement.UPDATE.getStatement().equals(lowercaseToken)) {
+                        formatted.append(indenter.newline());
+                        beginLine = true;
+                    }
+                }
+            } else if (StartClause.contains(lowercaseToken)) {
+                if (!inClauses) {
+                    if (MiscStatement.ON.getStatement().equals(lastToken)) {
+                        indenter.decrement();
+                    }
+
+                    formatted.append(indenter.decrement().newline());
+                }
+
+                inClauses = true;
+
+                formatted.append(token);
+                beginLine = false;
+
+            } else if (EndClause.contains(lowercaseToken)) {
+                if (!inClauses) {
+                    if (MiscStatement.ON.getStatement().equals(lastToken)) {
+                        indenter.decrement();
+                    }
+
+                    formatted.append(indenter.decrement().newline());
+                }
+
+                if (!EndClause.UNION.getClause().equals(lowercaseToken)) {
+                    indenter.increment();
+                }
+
+                formatted.append(token);
+                formatted.append(indenter.newline());
+                beginLine = true;
+                inClauses = false;
+
+                afterByOrSetOrFromOrSelect = EndClause.BY.getClause().equals(lowercaseToken)
+                        || EndClause.SET.getClause().equals(lowercaseToken)
+                        || EndClause.FROM.getClause().equals(lowercaseToken);
+
+            } else if (afterByOrSetOrFromOrSelect && Delimiter.comma().equals(token)) {
+                formatted.append(token);
+                formatted.append(indenter.newline());
+                beginLine = true;
+            } else if (MiscStatement.ON.getStatement().equals(lowercaseToken)) {
+                formatted.append(indenter.increment().newline());
+                formatted.append(token);
+                beginLine = false;
+            } else if (MiscStatement.ON.getStatement().equals(lastToken) & Delimiter.comma().equals(token)) {
+                formatted.append(token);
+                formatted.append(indenter.decrement().newline());
+                beginLine = true;
+                afterByOrSetOrFromOrSelect = true;
+            } else if ("(".equals(token)) {
+
+                startParenthesis.increment();
+
+                if (this.isFunctionName(lastToken) || function.isInFunction()) {
+                    function.increment();
+                }
+
+                if (function.isInFunction()) {
+                    formatted.append(token);
+                    beginLine = false;
+                } else {
+                    formatted.append(token);
+
+                    if (!afterByOrSetOrFromOrSelect) {
+                        formatted.append(indenter.increment().newline());
+                        beginLine = true;
+                    }
+                }
+            } else if (")".equals(token)) {
+
+                startParenthesis.decrement();
+
+                if (startParenthesis.hasParenthesis()) {
+                    indenter.decrement();
+                    startParenthesis.pop();
+                    afterByOrSetOrFromOrSelect = afterByOrFromOrSelects.removeLast();
+                }
+
+                if (function.isInFunction()) {
+                    function.decrement();
+                    formatted.append(token);
+                } else {
+                    if (!afterByOrSetOrFromOrSelect) {
+                        formatted.append(indenter.decrement().newline());
+                    }
+
+                    formatted.append(token);
+                }
+
+                beginLine = false;
+
+            } else if (EndClause.VALUES.getClause().equals(lowercaseToken)) {
+                formatted.append(indenter.decrement().newline());
+                formatted.append(token);
+                formatted.append(indenter.increment().newline());
+                beginLine = true;
+            } else if (LogicalExpression.contains(lowercaseToken)
+                    && !LogicalExpression.CASE.getExpression().equals(lowercaseToken)) {
+
+                if (LogicalExpression.END.getExpression().equals(lowercaseToken)) {
+                    indenter.decrement();
+                }
+
+                formatted.append(indenter.newline());
+                formatted.append(token);
+
+                beginLine = false;
+            } else if (Quantifier.BETWEEN.getQuantifier().equals(lastToken)
+                    && LogicalExpression.AND.getExpression().equals(lowercaseToken)) {
+
+                formatted.append(token);
+                beginLine = false;
+
+            } else if (this.isWhitespace(token)) {
+                if (!beginLine) {
+                    formatted.append(token);
+                }
+            } else {
+                formatted.append(token);
+
+                if (DmlStatement.INSERT.getStatement().equals(lastToken)) {
+                    formatted.append(indenter.newline());
+                    beginLine = true;
+                } else {
+                    beginLine = false;
+                    if (LogicalExpression.CASE.getExpression().equals(lowercaseToken)) {
+                        indenter.increment();
+                    }
+                }
+            }
         }
 
-        return new BasicFormatterImpl().format(sql);
+        return formatted.toString();
+    }
+
+    private boolean isFunctionName(@NonNull String token) {
+
+        final char begin = token.charAt(0);
+        final boolean isIdentifier = Character.isJavaIdentifierStart(begin) || '"' == begin;
+
+        return isIdentifier && !LogicalExpression.contains(token) && !EndClause.contains(token)
+                && !Quantifier.contains(token) && !DmlStatement.contains(token) && !MiscStatement.contains(token);
+    }
+
+    private boolean isWhitespace(@NonNull String token) {
+        return WHITESPACES.contains(token);
     }
 }
