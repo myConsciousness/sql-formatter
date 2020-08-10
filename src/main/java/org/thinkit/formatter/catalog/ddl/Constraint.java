b01c323aec7fea62dbe433eb3783ff50a1bbdaf2
Kato Shinya <yourdream28@gmail.com>
2020-08-10 15:25:57 +0900

    add

---
+++ src/main/java/org/thinkit/formatter/catalog/ddl/Constraint.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2020 Kato Shinya.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.thinkit.formatter.catalog.ddl;
+
+import org.thinkit.common.catalog.Catalog;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * DDL命令における制約を管理するカタログです。
+ *
+ * @author Kato Shinya
+ * @since 1.0
+ * @version 1.0
+ */
+@RequiredArgsConstructor
+public enum Constraint implements Catalog<Constraint> {
+
+    /**
+     * {@code foreign} 制約
+     */
+    FOREIGN(0, "foreign");
+
+    /**
+     * コード値
+     */
+    @Getter
+    private final int code;
+
+    /**
+     * 制約
+     */
+    @Getter
+    private final String constraint;
+}
